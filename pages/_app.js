d0b1470a32268fb95615a8329593e1b37cc78010
Diogo Carlos <dcarlos@relive.pt>
2022-08-06 12:03:59 +0100

    Revert local endpoint

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,7 @@ import { CookiesProvider, useCookies } from 'react-cookie';
 import CssBaseline from '@material-ui/core/CssBaseline'
 import axios from "axios";
 
-// axios.defaults.baseURL = process.env.LOCAL ? 'http://localhost:8080' : 'https://relive-api.herokuapp.com' /*https://relive-api.herokuapp.com*/
-axios.defaults.baseURL = 'http://localhost:5000'
+axios.defaults.baseURL = process.env.LOCAL ? 'http://localhost:8080' : 'https://relive-api.herokuapp.com' /*https://relive-api.herokuapp.com*/
 
 axios.interceptors.request.use(async function (response) {
     /* if (!axios.defaults.headers.common['Authorization']) {
